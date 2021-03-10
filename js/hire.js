c82d503de2a37f7fcedcfe61901ccc5772aa6ed2
alvynah <alvynahw@gmail.com>
2021-03-10 15:00:45 +0300

    Add iinitial service hire pages

---
+++ js/hire.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    //deluxeorder
+    $("#hireServicesButton").click(function() {
+        $("#hireServicesButton").hide();
+        $("#customerdetailsForm").slideDown(1000);
+    });
+});
