3f862bd41401bca3f42c3c2c961a5874def26938
alvynah <alvynahw@gmail.com>
2021-03-10 21:46:41 +0300

    Reset Initial commits

--- js/hire.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    //deluxeorder
-    $("#hireServicesButton").click(function() {
-        $("#hireServicesButton").hide();
-        $("#customerdetailsForm").slideDown(1000);
-    });
-});
