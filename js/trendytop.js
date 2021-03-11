cc057151ae84edb0a764a0ac6449662b070ec9bd
EnnyDin <78684712+EnnyDin@users.noreply.github.com>
2021-03-11 08:38:03 +0300

    removed unused code (#11)

--- js/trendytop.js
+++
@@ -1,8 +0,0 @@
-
-	            if (e.direction=="left") {
-	                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-	            }
-	            else {
-	                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-	            }
-	        
