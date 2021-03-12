8ac0069a4a158b17b9efa496596c51f1927cef47
alvynah <alvynahw@gmail.com>
2021-03-12 08:03:32 +0300

    Display workers in a table using a fetch API

--- js/hire.js
+++ js/hire.js
@@ -1,16 +1,42 @@
 //Business Logic
-
-if (!('fetch' in window)) {
-    console.log('Fetch API not found, try including the polyfill');
-    return;
-}
-fetch('examples/example.json')
+fetch("../js/hire.json")
     .then(function(response) {
-        // Do stuff with the response
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
     })
-    .catch(function(error) {
-        console.log('Looks like there was a problem: \n', error);
-    });
+    .then(function(data) {
+        console.log(data);
+
+        var jsonData = data.plumbers;
+        if (jsonData.length > 0) {
+            var temp = "";
+
+            jsonData.forEach(function(u) {
+                temp += "<tr>";
+                temp += "<td>" + u.userId + "</td>";
+                temp += "<td>" + u.firstName + "</td>";
+                temp += "<td>" + u.lastName + "</td>";
+                temp += "<td>" + u.phoneNumber + "</td>";
+                temp += "<td>" + u.emailAddress + "</td>";
+                temp += "<td>" + u.location + "</td>";
+                temp += "<td>" + u.rates + "</td>";
+                temp += "<td>" + u.ratings + "</td>";
+                temp += "<td>" + "<input type='checkbox'>" + "</td>";
+
+            });
+            document.getElementById("servicesOrderedBodyTable").innerHTML = temp;
+        }
+    })
+
+.catch(function(error) {
+    console.log('Looks like there was a problem: \n', error);
+});
+
+
+
+
 //User Logic
 $(document).ready(function() {
     //deluxeorder
@@ -18,11 +44,18 @@ $(document).ready(function() {
         $("#hireServicesButton").hide();
         $("#customerdetailsForm").slideDown(1000);
     });
+
     $("form#customerdetailsForm").submit(function(event) {
         event.preventDefault();
 
         $("#serviceOrdered").slideDown(1000);
 
-    })
+        $("#name").val("");
+        $("#phone").val("");
+        $("#location").val("");
+        $("#serviceSelect").val("0");
+        $("#description").val("");
+    });
+
 
 });
