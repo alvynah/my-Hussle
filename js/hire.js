adb6df669b638914f4d0f1ee02316f06abe3bed3
alvynah <alvynahw@gmail.com>
2021-03-12 05:10:52 +0300

    Add table to dsplay json content

--- js/hire.js
+++ js/hire.js
@@ -1,3 +1,17 @@
+//Business Logic
+
+if (!('fetch' in window)) {
+    console.log('Fetch API not found, try including the polyfill');
+    return;
+}
+fetch('examples/example.json')
+    .then(function(response) {
+        // Do stuff with the response
+    })
+    .catch(function(error) {
+        console.log('Looks like there was a problem: \n', error);
+    });
+//User Logic
 $(document).ready(function() {
     //deluxeorder
     $("#hireServicesButton").click(function() {
@@ -7,7 +21,8 @@ $(document).ready(function() {
     $("form#customerdetailsForm").submit(function(event) {
         event.preventDefault();
 
-        $("#customerdetailsForm").hide();
-        $("#hireServicesButton").slideUp(1000);
+        $("#serviceOrdered").slideDown(1000);
+
     })
+
 });
